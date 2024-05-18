df6e655b7c30801f87e54748cfcee07035654cd8
Thomas <thomas.blakely@gmx.com>
2024-05-18 22:18:30 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,164 +1,163 @@
-const players = [{
-  name: 'inertiacreeping',
-  twitch: 'https://www.twitch.tv/inertiacreeping',
-  discordName: 'Low ELO Legends',
-  discordLink: 'https://discord.gg/XXvQE4uY2m',
-  steamID: '/steam/76561198067966567'
-},
-{
-  name: 'shiderplays',
-  twitch: 'https://www.twitch.tv/shiderplays',
-  discordName: 'ShiderPlays',
-  discordLink: 'https://discord.gg/dzXthPsvKQ',
-  steamID: '/steam/76561198151727332'
-},
-{
-  name: 'tambow',
-  twitch: 'https://www.twitch.tv/ttambow',
-  discordName: 'tambourine town',
-  discordLink: 'https://discord.gg/yfyw3ZRUua',
-  steamID: '/steam/76561198040642066'
-},
-{
-  name: 'chestnutplace77',
-  twitch: 'https://www.twitch.tv/chestnutplace77',
-  discordName: 'the nutshack',
-  discordLink: 'https://discord.gg/BGEA4HxY8u',
-  steamID: '/steam/76561198403361404'
-}];
+    const players = [{
+        name: 'inertiacreeping',
+        twitch: 'https://www.twitch.tv/inertiacreeping',
+        discordName: 'Low ELO Legends',
+        discordLink: 'https://discord.gg/XXvQE4uY2m',
+        steamID: '/steam/76561198067966567'
+    },
+    {
+        name: 'shiderplays',
+        twitch: 'https://www.twitch.tv/shiderplays',
+        discordName: 'ShiderPlays',
+        discordLink: 'https://discord.gg/dzXthPsvKQ',
+        steamID: '/steam/76561198151727332'
+    },
+    {
+        name: 'tambow',
+        twitch: 'https://www.twitch.tv/ttambow',
+        discordName: 'tambourine town',
+        discordLink: 'https://discord.gg/yfyw3ZRUua',
+        steamID: '/steam/76561198040642066'
+    },
+    {
+        name: 'chestnutplace77',
+        twitch: 'https://www.twitch.tv/chestnutplace77',
+        discordName: 'the nutshack',
+        discordLink: 'https://discord.gg/BGEA4HxY8u',
+        steamID: '/steam/76561198403361404'
+    }];
 
-let fireworksInstance;
+    let fireworksInstance;
 
-function playAudio() {
-  const audio = document.getElementById('wololo-audio');
-  audio.volume = 0.3; // Set the volume to 30%
-  audio.play().catch(error => console.error('Audio play failed:', error));
-  triggerFireworks();
-  toggleBlueTheme();
-}
-
-function triggerFireworks() {
-  const container = document.getElementById('fireworks-container');
-  if (!fireworksInstance) {
-    fireworksInstance = new Fireworks(container, {
-      maxRockets: 15, // max # of rockets to spawn
-      rocketSpawnInterval: 150, // milliseconds to check if new rockets should spawn
-      numParticles: 100, // number of particles to spawn when rocket explodes
-      explosionMinHeight: 0.2,
-      explosionMaxHeight: 0.9,
-      explosionChance: 0.08 // chance in each tick the rocket will explode
-  });
-}
-
-fireworksInstance.start();
-
-setTimeout(() => {
-  fireworksInstance.stop();
-  }, 10000);
-}
-
-function toggleBlueTheme() {
-  document.body.classList.toggle('blue-theme');
-}
-
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
-async function generatePlayerList() {
-  const shuffledPlayers = shuffleArray(players);
-  const playerListContainer = document.getElementById('player-list');
-  playerListContainer.innerHTML = '';
-
-  for (const player of shuffledPlayers) {
-    const playerElement = await createPlayerElement(player);
-    playerListContainer.appendChild(playerElement);
-  }
-}
-
-async function createPlayerElement(player) {
-  try {
-    const response = await fetch('player-template.html')
-    const template = await response.text();
-    const playerHTML = template
-      .replace('{{playerName}}', player.name)
-      .replace('{{steamID}}', player.steamID)
-      .replace('{{twitch}}', player.twitch)
-      .replace('{{discordLink}}', player.discordLink)
-      .replace('{{discordName}}', player.discordName);
-        
-    const playerElement = document.createElement('div');
-    playerElement.classList.add('player');
-    playerElement.innerHTML = playerHTML;
-    return playerElement;
-  } catch(error) {
-    console.error("Error creating player element: ", error);
-    throw error;
-  }
-}
-
-async function fetchEloData() {
-  try {
-    const response = await fetch('/elo_data.json');
-    const data = await response.json();
-    data.forEach(playerData => {
-      document.getElementById(`elo-1v1-${playerData.steamID}`).textContent = `1v1 ELO: ${playerData.elo1v1}`;
-      document.getElementById(`elo-team-${playerData.steamID}`).textContent = `Team ELO: ${playerData.eloTeam}`;
-    });
-  } catch (error) {
-    console.error('Error fetching ELO data:', error);
-    players.forEach(player => {
-      document.getElementById(`elo-1v1-${player.steamID}`).textContent = '1v1 ELO: N/A';
-      document.getElementById(`elo-team-${player.steamID}`).textContent = 'Team ELO: N/A';
-    });
-  }
-}
-
-async function checkLiveStatus() {
-  for (const player of players) {
-    const statusElement = document.getElementById(`twitch-${player.name}`);
-    if (!statusElement) {
-      console.error(`Element with ID twitch-${player.name} not found`);
-    continue;
-  }
-  try {
-    const response = await fetch(`https://api.twitch.tv/helix/streams?user_login=${player.name}`, {
-      headers: {
-        'Client-ID': 'wsp888x940dnqxdecdjmcrrz5kflb5',
-        'Authorization': `Bearer oy5z377gdk6qjwrswqh3usp034gof7`
-      }
-    });
-    const data = await response.json();
-    console.log(`Streamer: ${player.name}`, data); // Log the data for each streamer
-    const statusSpan = statusElement.querySelector('.live-text');
-    if (data.data && data.data.length > 0 && data.data[0].type === 'live') {
-      statusSpan.textContent = 'LIVE';
-      statusSpan.classList.add('flash');
-      statusElement.classList.remove('offline');
-      statusElement.classList.add('live');
-    } else {
-      statusSpan.textContent = 'OFFLINE';
-      statusSpan.classList.remove('flash');
-      statusElement.classList.remove('live');
-      statusElement.classList.add('offline');
+    function playAudio() {
+        const audio = document.getElementById('wololo-audio');
+        audio.volume = 0.3; // Set the volume to 30%
+        audio.play().catch(error => console.error('Audio play failed:', error));
+        triggerFireworks();
+        toggleBlueTheme();
     }
-  } catch (error) {
-    console.error(`Error fetching status for ${player.name}:`, error);
-    const statusSpan = statusElement.querySelector('.live-text');
-    statusSpan.textContent = 'ERROR';
-    statusSpan.classList.remove('flash');
-    statusElement.classList.remove('live', 'offline');
-    statusElement.style.color = 'orange';
-  }}
-}
 
-document.addEventListener('DOMContentLoaded', () => {
-  generatePlayerList();
-  fetchEloData();
-  checkLiveStatus();
-  setInterval(checkLiveStatus, 30000); // Check every 30 seconds
-});
+    function triggerFireworks() {
+        const container = document.getElementById('fireworks-container');
+        if (!fireworksInstance) {
+            fireworksInstance = new Fireworks(container, {
+                maxRockets: 15, // max # of rockets to spawn
+                rocketSpawnInterval: 150, // milliseconds to check if new rockets should spawn
+                numParticles: 100, // number of particles to spawn when rocket explodes
+                explosionMinHeight: 0.2,
+                explosionMaxHeight: 0.9,
+                explosionChance: 0.08 // chance in each tick the rocket will explode
+            });
+        }
+        
+        fireworksInstance.start();
+
+        setTimeout(() => {
+            fireworksInstance.stop();
+        }, 10000);
+    }
+
+    function toggleBlueTheme() {
+        document.body.classList.toggle('blue-theme');
+    }
+
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    function generatePlayerList() {
+        const shuffledPlayers = shuffleArray(players);
+        const playerListContainer = document.getElementById('player-list');
+        playerListContainer.innerHTML = '';
+        shuffledPlayers.forEach(player => {
+            const playerElement = document.createElement('div');
+            playerElement.classList.add('player');
+            playerElement.innerHTML = `
+            <div class="player-info">
+            <div>
+                <h2>${player.name}</h2>
+                <p class="elo" id="elo-1v1-${player.steamID}">1v1 ELO: Loading...</p>
+                <p class="elo" id="elo-team-${player.steamID}">Team ELO: Loading...</p>
+            </div>
+            <div>
+                <a href="${player.twitch}" target="_blank" class="twitch-button offline" id="twitch-${player.name}">
+                <img src="https://github.com/inertiacreeping/wololo/blob/main/twitch.png?raw=true" class="icon" alt="Twitch">
+                <span class="live-text">OFFLINE</span>
+                </a>
+            </div>
+            <div>
+                <a href="${player.discordLink}" target="_blank" class="discord-button">
+                <img src="https://github.com/inertiacreeping/wololo/blob/main/discord.png?raw=true" class="icon" alt="Discord">
+                <span>${player.discordName}</span>
+                </a>
+            </div>
+            </div>
+            `;
+            playerListContainer.appendChild(playerElement);
+        });
+     }
+
+    async function fetchEloData() {
+    try {
+        const response = await fetch('/elo_data.json');
+        const data = await response.json();
+        data.forEach(playerData => {
+            document.getElementById(`elo-1v1-${playerData.steamID}`).textContent = `1v1 ELO: ${playerData.elo1v1}`;
+            document.getElementById(`elo-team-${playerData.steamID}`).textContent = `Team ELO: ${playerData.eloTeam}`;
+        });
+    } catch (error) {
+        console.error('Error fetching ELO data:', error);
+        players.forEach(player => {
+            document.getElementById(`elo-1v1-${player.steamID}`).textContent = '1v1 ELO: N/A';
+            document.getElementById(`elo-team-${player.steamID}`).textContent = 'Team ELO: N/A';
+        });
+    }}
+
+    async function checkLiveStatus() {
+        for (const player of players) {
+            const statusElement = document.getElementById(`twitch-${player.name}`);
+            if (!statusElement) {
+                console.error(`Element with ID twitch-${player.name} not found`);
+                continue;
+            }
+
+        try {
+            const response = await fetch(`https://api.twitch.tv/helix/streams?user_login=${player.name}`, {
+                headers: {
+                    'Client-ID': 'wsp888x940dnqxdecdjmcrrz5kflb5',
+                    'Authorization': `Bearer oy5z377gdk6qjwrswqh3usp034gof7`
+                }
+            });
+            const data = await response.json();
+            console.log(`Streamer: ${player.name}`, data); // Log the data for each streamer
+            const statusSpan = statusElement.querySelector('.live-text');
+            if (data.data && data.data.length > 0 && data.data[0].type === 'live') {
+                statusSpan.textContent = 'LIVE';
+                statusSpan.classList.add('flash');
+                statusElement.classList.remove('offline');
+                statusElement.classList.add('live');
+            } else {
+                statusSpan.textContent = 'OFFLINE';
+                statusSpan.classList.remove('flash');
+                statusElement.classList.remove('live');
+                statusElement.classList.add('offline');
+            }
+        } catch (error) {
+            console.error(`Error fetching status for ${player.name}:`, error);
+            const statusSpan = statusElement.querySelector('.live-text');
+            statusSpan.textContent = 'ERROR';
+            statusSpan.classList.remove('flash');
+            statusElement.classList.remove('live', 'offline');
+            statusElement.style.color = 'orange';
+        }}
+    }
+    document.addEventListener('DOMContentLoaded', () => {
+        generatePlayerList();
+        fetchEloData();
+        checkLiveStatus();
+        setInterval(checkLiveStatus, 30000); // Check every 30 seconds
+    });
