3403f94093caf2a36e8ae7fc2c0ec404fe9dec5a
Thomas <thomas.blakely@gmx.com>
2024-05-30 23:23:36 +1200

    go go go

--- scripts/elo.js
+++ scripts/elo.js
@@ -1,21 +1,19 @@
 import players from './playersData.js';
 
-export async function fetchEloData() {
-    const title = "age2";
-    for (const player of players) {
-        const profile_names = player.steamId; 
-        const eloApiUrl = "https://aoe-api.reliclink.com/community/leaderboard/GetPersonalStat"; 
-        const params = {
-            title,
-            profile_names
-        };
 
-        try {
-            const response = await fetch(eloApiUrl, {params});
-            const data = await response.json();
-            console.log(data);
-        } catch (error) {
-            console.error(`Error fetching elo for ${player.name}: `, error);
-        }
+export async function fetchEloData() {
+    try {
+        const response = await fetch('../data/elo_data.json');
+        const data = await response.json();
+        data.forEach(players => {
+            document.getElementById(`elo-1v1-${players.steamId}`).textContent = `1v1 ELO: ${players.elo1v1}`;
+            document.getElementById(`elo-team-${players.steamId}`).textContent = `Team ELO: ${players.eloTeam}`;
+        });
+    } catch (error) {
+        console.error("Error fetching ELO data: ", error);
+        players.forEach(player => {
+            document.getElementById(`elo-1v1-${player.steamId}`).textContent = "1v1 ELO: N/A";
+            document.getElementById(`elo-team-${player.steamId}`).textContent = "Team ELO: N/A";
+        });
     }
 }
